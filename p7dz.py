271c7137072b3de1e90ac16f606e771cfda3cfd5
ilazaac <ilazaac4@gmail.com>
2022-11-12 14:09:58 +0200

    зробив 1 завдання так, як зрозумів

--- p7dz.py
+++ p7dz.py
@@ -1,17 +1,17 @@
 import random
 
 
-def exponentiation(num):
-    i = 0
+def multiplier(num):
+    a = 0
     while True:
-        result = num ** i
+        result = num ** a
         yield result
         if result > 100**20:
             break
-        i += 1
+        a += 1
 
 
-iterator = iter(exponentiation(random.randint(2, 10)))
+iterator = iter(multiplier(random.randint(2, 10)))
 for elem in iterator:
     print(elem)
     print('------')
